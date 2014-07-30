0300ccd5a33538f946b71ced48b53e56c62dc4fd
Bill Noon <wn10@cornell.edu>
2014-07-30 15:32:37 -0400

    Add support config files

---
+++ dataset_defs.py
@@ -0,0 +1,3 @@
+## Dataset Namespace
+##----------------
+from mowx import MOwxStation as Station, MissingInfo, vXvN, network_id, id_type, zlog_formats
---
+++ local_defs_proto.py
@@ -0,0 +1,19 @@
+## Local Settings
+##----------------
+data_path = '../data'
+miss_path = '../miss'
+meta_path = '../meta'
+upd_path = '.'
+zlog_path = '../zlog'
+zlog_src = 'mrcc'
+
+# raw mrcc data
+src_qual_flag = 0x10000000
+
+# Postgres settings
+sql_db_host = None
+sql_db_user = 'data'
+sql_db_name = 'ucan_station'
+
+feed_name = 'mowx'
+update_furl = 'sync.furl'
